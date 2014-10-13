cac05234d90f98c565b4b8fe583f3a4e83b40ad0
Dennis Middendorp <D-Middendorp@users.noreply.github.com>
2014-10-13 10:41:34 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,16 +16,17 @@
  	"wget":                     ensure => installed;
  	"tar":                      ensure => installed;
     	"bwa":                      ensure => installed;
+    	"zlib-devel":		    ensure => installed;
 
 }
 
 # set default paths for storing data, tools and source code
 $username = "hettling"
 $erycina_dir	= "/home/${username}/assembly_Erycina"  ##"/home/${id}/assembly_Erycina"
-$tools_dir		= "${erycina_dir}/tools"
+$tools_dir	= "${erycina_dir}/tools"
 $tools_bin_dir	= "${tools_dir}/bin"
-$src_dir		= "${erycina_dir}/src"
-$data_dir		= "${erycina_dir}/data"
+$src_dir	= "${erycina_dir}/src"
+$data_dir	= "${erycina_dir}/data"
 
 # create links for executables and data directories
 file {
@@ -36,27 +37,27 @@ file {
 		recurse => true;
 	$data_dir:
 		ensure  => directory,
-    group   => $username,
-    owner   => $username,
-    recurse => true;
+    		group   => $username,
+    		owner   => $username,
+		recurse => true;
 	$src_dir:
 		ensure  => directory,
-    group   => $username,
-    owner   => $username,
-    recurse => true;
+    		group   => $username,
+    		owner   => $username,
+    		recurse => true;
 	$tools_dir:
 		ensure  => directory,
-	  group   => $username,
-    owner   => $username,
-    recurse => true;
+	  	group   => $username,
+    		owner   => $username,
+    		recurse => true;
 
-  $tools_bin_dir:
+  	$tools_bin_dir:
 		ensure  => directory,
-	  group   => $username,
-    owner   => $username,
-    recurse => true;
+	  	group   => $username,
+    		owner   => $username,
+    		recurse => true;
 
-  "muscle_link":
+  	"muscle_link":
 		path    => "${tools_bin_dir}/muscle",
 		ensure  => link,
 		target  => "${tools_bin_dir}/muscle3.8.31_i86linux64",
@@ -71,13 +72,13 @@ file {
 		ensure  => link,
 		target  => "${tools_dir}/ExaML/examl/examl",
 		require => Exec["compile_examl"];
-    "exabayes_link":
+    	"exabayes_link":
 		path    => "/usr/local/bin/exabayes",
 		ensure  => link,
 		target  => "${tools_dir}/exabayes-1.2.1/bin/exabayes",
 		require => Exec["compile_exabayes"];
-     "treepl_link":
-	    path    => "/usr/local/bin/treePL",
+        "treepl_link":
+	    	path    => "/usr/local/bin/treePL",
 		ensure  => link,
 		target  => "${tools_dir}/treePL/src/treePL",
 		require => Exec["compile_treepl"];
@@ -88,7 +89,6 @@ file {
 
 # command line tasks
 exec {
-
   # add bin directory for all required tools to PATH
   "make_bindir_sh":
     command => "echo 'export PATH=\$PATH:${tools_bin_dir}' > supersmart-tools-bin.sh",
@@ -100,15 +100,13 @@ exec {
     cwd     => "/etc/profile.d",
     creates => "/etc/profile.d/supersmart-tools-bin.csh",
     require => File[ $tools_bin_dir ];
-
-
 	# install bwa
 	"download_bwa":
 		command => "wget http://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.10.tar.bz2 -O bwa-0.7.10.tar.bz2",
 		cwd     => $tools_dir,
 		creates => "${tools_dir}/bwa-0.7.10.tar.bz2",
 		require => Package[ 'wget', 'tar' ];
-    "bunzip_bwa":
+        "bunzip_bwa":
 		command => "bunzip2 bwa-0.7.10.tar.bz2",
 		cwd     => $tools_dir,
 		creates => "${tools_dir}/bwa-0.7.10.tar.bz2",
